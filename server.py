bdde25eac5b21205f7e8ddc1758decff8e753e59
Haicheng Charles Zhao <czhao39@gmail.com>
2018-06-23 14:08:53 -0400

    Convert and save PDF as PNG

--- server.py
+++ server.py
@@ -3,6 +3,7 @@
 import os
 from flask import *
 from werkzeug.utils import secure_filename
+from wand.image import Image, Color
 
 UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), "uploads/")
 print(UPLOAD_FOLDER)
@@ -19,6 +20,7 @@ def is_allowed_file(filename):
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
+        # Save PDF
         if "file" not in request.files:
             flash("No file uploaded.")
             return redirect(request.url)
@@ -29,9 +31,27 @@ def index():
         if not is_allowed_file(pdf_file.filename):
             flash("Invalid file. Must be a PDF.")
             return redirect(request.url)
-        file_name = secure_filename(pdf_file.filename)
-        pdf_file.save(os.path.join(app.config["UPLOAD_FOLDER"], file_name))
-        # TODO redirect to divider page
+        pdf_filename = secure_filename(pdf_file.filename)
+        path_to_pdf = os.path.join(app.config["UPLOAD_FOLDER"], pdf_filename)
+        pdf_file.save(path_to_pdf)
+
+        # Convert to and save PNG
+        png_filename = "{}.png".format(os.path.splitext(pdf_filename)[0])
+        with Image(filename=path_to_pdf) as pdf_image:
+            num_pages = len(pdf_image.sequence)
+            with Image(width=pdf_image.width, height=pdf_image.height * num_pages) as png_image:
+                for p in range(num_pages):
+                    png_image.composite(
+                        pdf_image.sequence[p],
+                        top=pdf_image.height * p,
+                        left=0
+                    )
+                png_image.format = "png"
+                png_image.background_color = Color("white")
+                png_image.alpha_channel = "remove"
+                png_image.save(filename=os.path.join(app.config["UPLOAD_FOLDER"], png_filename))
+
+        # TODO: redirect to divider page
     return render_template("submit_pdf.html")
 
 
