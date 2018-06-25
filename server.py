533190280165e7e048c9880dff7c9b69678aa1e7
Haicheng Charles Zhao <czhao39@gmail.com>
2018-06-24 22:16:18 -0400

    Create divider page that displays submission as PNG

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import base64
+from io import BytesIO
 import os
 from flask import *
 from werkzeug.utils import secure_filename
@@ -10,6 +12,7 @@ print(UPLOAD_FOLDER)
 ALLOWED_EXTENSIONS = {"pdf"}
 
 app = Flask(__name__, static_url_path="")
+app.secret_key = b"\xd9h\xf8\xd2-\x8c\xe6\xdc;\r\xd9\x10'[\x03;"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 
@@ -17,42 +20,47 @@ def is_allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def index():
-    if request.method == "POST":
-        # Save PDF
-        if "file" not in request.files:
-            flash("No file uploaded.")
-            return redirect(request.url)
-        pdf_file = request.files["file"]
-        if not pdf_file or pdf_file.filename == "":
-            flash("No file selected.")
-            return redirect(request.url)
-        if not is_allowed_file(pdf_file.filename):
-            flash("Invalid file. Must be a PDF.")
-            return redirect(request.url)
-        pdf_filename = secure_filename(pdf_file.filename)
-        path_to_pdf = os.path.join(app.config["UPLOAD_FOLDER"], pdf_filename)
-        pdf_file.save(path_to_pdf)
-
-        # Convert to and save PNG
-        png_filename = "{}.png".format(os.path.splitext(pdf_filename)[0])
-        with Image(filename=path_to_pdf) as pdf_image:
-            num_pages = len(pdf_image.sequence)
-            with Image(width=pdf_image.width, height=pdf_image.height * num_pages) as png_image:
-                for p in range(num_pages):
-                    png_image.composite(
-                        pdf_image.sequence[p],
-                        top=pdf_image.height * p,
-                        left=0
-                    )
-                png_image.format = "png"
-                png_image.background_color = Color("white")
-                png_image.alpha_channel = "remove"
-                png_image.save(filename=os.path.join(app.config["UPLOAD_FOLDER"], png_filename))
-
-        # TODO: redirect to divider page
     return render_template("submit_pdf.html")
+
+
+@app.route("/divider", methods=["POST"])
+def divider():
+    # Save PDF
+    if "file" not in request.files:
+        flash("No file uploaded.")
+        return redirect(request.url)
+    pdf_file = request.files["file"]
+    if not pdf_file or pdf_file.filename == "":
+        flash("No file selected.")
+        return redirect(request.url)
+    if not is_allowed_file(pdf_file.filename):
+        flash("Invalid file. Must be a PDF.")
+        return redirect(request.url)
+    pdf_filename = secure_filename(pdf_file.filename)
+    path_to_pdf = os.path.join(app.config["UPLOAD_FOLDER"], pdf_filename)
+    pdf_file.save(path_to_pdf)
+
+    # Convert to and save PNG to stream
+    png_stream = BytesIO()
+    with Image(filename=path_to_pdf) as pdf_image:
+        num_pages = len(pdf_image.sequence)
+        with Image(width=pdf_image.width, height=pdf_image.height * num_pages) as png_image:
+            for p in range(num_pages):
+                png_image.composite(
+                    pdf_image.sequence[p],
+                    top=pdf_image.height * p,
+                    left=0
+                )
+                png_image.format = "png"
+            png_image.background_color = Color("white")
+            png_image.alpha_channel = "remove"
+            png_image.save(file=png_stream)
+
+    # Return PNG in base 64
+    png_b64 = base64.b64encode(png_stream.getvalue()).decode("ascii")
+    return render_template("divider.html", image_b64=png_b64)
 
 
 if __name__ == "__main__":
