a9524f38ab68c5e4966e11f88620046ce77f1b76
Haicheng Charles Zhao <czhao39@gmail.com>
2018-07-01 20:42:06 -0400

    Add scrollspy

--- static/js/submission_panel.js
+++ static/js/submission_panel.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
     var $submissionContainer = $("#submission-container");
+    var $panelBody = $("#submission-panel > .panel-body");
     $("#submission-zoomin-btn").click(function() {
         $submissionContainer.width($submissionContainer.width() * 1.1);
+        $panelBody.scrollspy("refresh");
     });
     $("#submission-zoomout-btn").click(function() {
         $submissionContainer.width($submissionContainer.width() / 1.1);
+        $panelBody.scrollspy("refresh");
     })
 });
 
